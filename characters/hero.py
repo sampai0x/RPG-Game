5c84f11996eef0acb3587a178540a424e88ac660
sampai0x <ydarkpvp150934@gmail.com>
2026-01-20 20:32:11 -0300

    Characters add

---
+++ characters/hero.py
@@ -0,0 +1,13 @@
+class Hero:
+    def __init__(self, name, hp, attack):
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+    
+    def is_alive(self):
+        return self.hp > 0
+    
+    def take_damage(self, damage):
+        self.hp -= damage
+        if self.hp < 0:
+            self.hp = 0
